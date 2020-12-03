2d0b08613ec9e5968bf10096dcd8c6629f804da0
Abhishek Jog <49052188+abhishekjog@users.noreply.github.com>
2020-12-04 00:03:31 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -x
+
+#!/bin/bash -eux
+
+GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.14.linux-amd64.tar.gz"
+wget --no-check-certificate --progress=dot:mega ${GO_LINUX_PACKAGE_URL} -O go-linux.tar.gz 
+tar -zxf go-linux.tar.gz
+mv go /usr/local/
+mkdir -p /go/bin /go/src /go/pkg
+
+export GO_HOME=/usr/local/go
+export GOPATH=/go
+export PATH=${GOPATH}/bin:${GO_HOME}/bin/:$PATH
+
+THIS_GITHUB_EVENT=$(cat $GITHUB_EVENT_PATH)
+RELEASE_UPLOAD_URL=$(echo $THIS_GITHUB_EVENT | jq -r .release.upload_url)
+RELEASE_UPLOAD_URL=${RELEASE_UPLOAD_URL/\{?name,label\}/}
+RELEASE_TAG_NAME=$(echo $THIS_GITHUB_EVENT | jq -r .release.tag_name)
+PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
+
+EXECUTABLE_FILES=`/build.sh`
+EXECUTABLE_FILES=`echo "${EXECUTABLE_FILES}" | awk '{$1=$1};1'`
+
+PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
+TMP_ARCHIVE=tmp.tgz
+tar cvfz $TMP_ARCHIVE --directory ${PROJECT_ROOT}/${SUBDIR} ${EXECUTABLE_FILES} 
+
+CKSUM=$(md5sum ${TMP_ARCHIVE} | cut -d ' ' -f 1)
+NAME="${NAME:-${EXECUTABLE_FILES}_${RELEASE_TAG_NAME}}_${GOOS}_${GOARCH}"
+
+curl \
+  -X POST \
+  --data-binary @${TMP_ARCHIVE} \
+  -H 'Content-Type: application/octet-stream' \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "${RELEASE_UPLOAD_URL}?name=${NAME}.${TMP_ARCHIVE/tmp./}"
+
+curl \
+  -X POST \
+  --data $CKSUM \
+  -H 'Content-Type: text/plain' \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "${RELEASE_UPLOAD_URL}?name=${NAME}_CKSUM.txt"
